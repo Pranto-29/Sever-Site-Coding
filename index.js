14bb1250cb27fae1139c13acde431484f377bbde
Pranto-29 <23pranto729@gmail.com>
2025-12-03 18:28:57 +0600

    added an section

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const db = client.db('Model-db');
 
     const modelCollection = db.collection('models');
